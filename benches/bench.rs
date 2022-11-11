9767ffd25a2731cc76f199238efcb7c995dd85a8
Toru3 <ryuutet@gmail.com>
2022-11-12 02:05:25 +0900

    initial commit

---
+++ benches/bench.rs
@@ -0,0 +1,126 @@
+use criterion::{
+    black_box, criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion,
+    PlotConfiguration, Throughput,
+};
+use momen::*;
+use rand::Rng;
+use rayon::prelude::*;
+
+fn copy_ref(x: &[f64], y: &mut [f64]) {
+    y.iter_mut().zip(x.iter()).for_each(|(y, x)| *y = *x);
+}
+
+fn copy_aux(arg: &mut (&[f64], &mut [f64])) {
+    copy_ref(arg.0, arg.1);
+}
+
+fn copy_rayon(x: &[f64], y: &mut [f64]) {
+    y.par_iter_mut()
+        .zip(x.par_iter())
+        .for_each(|(y, x)| *y = *x);
+}
+
+fn gen_rand(n: usize) -> Vec<f64> {
+    let mut rng = rand::thread_rng();
+    (0..n).map(|_| rng.gen()).collect()
+}
+
+fn check(x: &[f64], y: &[f64]) -> bool {
+    if x.len() != y.len() {
+        return false;
+    }
+    for (xi, yi) in x.iter().zip(y.iter()) {
+        if xi != yi {
+            return false;
+        }
+    }
+    true
+}
+
+fn bench_copy(c: &mut Criterion) {
+    let cool_down = std::time::Duration::from_secs(3);
+    std::thread::sleep(cool_down);
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
+    let mut group = c.benchmark_group("copy");
+    group.plot_config(plot_config);
+    let pool1 = ThreadPool::new(black_box(copy_aux));
+    let pool2 = ThreadPoolDyn::new();
+    for n in 11..22 {
+        let size = 1 << n;
+        let nproc = std::thread::available_parallelism().unwrap().get();
+        let batch_size = (size + nproc - 1) / nproc;
+        group.throughput(Throughput::Bytes(
+            (2 * std::mem::size_of::<f64>() * size) as u64,
+        ));
+        {
+            let x = gen_rand(size);
+            let mut y = vec![0.; size];
+            group.bench_with_input(BenchmarkId::new("Reference", size), &size, |b, &_size| {
+                b.iter(|| copy_ref(black_box(&x), black_box(&mut y)));
+            });
+            assert!(check(&x, &y));
+            std::thread::sleep(cool_down);
+        }
+        {
+            let x = gen_rand(size);
+            let mut y = vec![0f64; size];
+            let mut input = x
+                .chunks(batch_size)
+                .zip(y.chunks_mut(batch_size))
+                .collect::<Vec<_>>();
+            group.bench_with_input(BenchmarkId::new("ThreadPool", size), &size, |b, &_size| {
+                b.iter(|| pool1.run(black_box(&mut input)));
+            });
+            assert!(check(&x, &y));
+            std::thread::sleep(cool_down);
+        }
+        {
+            let x = gen_rand(size);
+            let mut y = vec![0f64; size];
+            let mut input = x
+                .chunks(batch_size)
+                .zip(y.chunks_mut(batch_size))
+                .collect::<Vec<_>>();
+            group.bench_with_input(
+                BenchmarkId::new("ThreadPoolDyn", size),
+                &size,
+                |b, &_size| {
+                    b.iter(|| pool2.run(black_box(&mut input), black_box(&copy_aux)));
+                },
+            );
+            assert!(check(&x, &y));
+            std::thread::sleep(cool_down);
+        }
+        {
+            let x = gen_rand(size);
+            let mut y = vec![0.; size];
+            group.bench_with_input(BenchmarkId::new("Rayon", size), &size, |b, &_size| {
+                b.iter(|| copy_rayon(black_box(&x), black_box(&mut y)));
+            });
+            assert!(check(&x, &y));
+        }
+        {
+            let x = gen_rand(size);
+            let mut y = vec![0.; size];
+            group.bench_with_input(BenchmarkId::new("Rayon chunk", size), &size, |b, &_size| {
+                b.iter(|| {
+                    black_box(&x)
+                        .par_chunks(batch_size)
+                        .zip(black_box(&mut y).par_chunks_mut(batch_size))
+                        .for_each(|(x, y)| copy_ref(x, y));
+                });
+            });
+            assert!(check(&x, &y));
+            std::thread::sleep(cool_down);
+        }
+    }
+    group.finish();
+}
+
+criterion_group! {
+    name = copy;
+    config = Criterion::default()
+        .sample_size(10000);
+    targets = bench_copy
+}
+criterion_main!(copy);
---
+++ src/lib.rs
@@ -0,0 +1,344 @@
+#![doc = include_str!("../README.md")]
+use clone_all::clone_all;
+use core::{marker::PhantomData, sync::atomic::AtomicUsize};
+use std::{
+    sync::Arc,
+    thread::{self, JoinHandle},
+};
+
+type Futex = core::sync::atomic::AtomicU32;
+type FutexInt = u32;
+type PhantomUnsync = PhantomData<core::cell::Cell<()>>;
+
+macro_rules! load {
+    ($v:expr) => {
+        $v.load(core::sync::atomic::Ordering::Acquire)
+    };
+}
+macro_rules! store {
+    ($v:expr, $e:expr) => {
+        $v.store($e, core::sync::atomic::Ordering::Release)
+    };
+}
+unsafe fn inc(x: &Futex) {
+    store!(x, load!(x).wrapping_add(1));
+}
+fn load_pair(x: &[AtomicUsize; 2]) -> [usize; 2] {
+    [load!(x[0]), load!(x[1])]
+}
+fn store_pair(dst: &[AtomicUsize; 2], x: [usize; 2]) {
+    store!(dst[0], x[0]);
+    store!(dst[1], x[1]);
+}
+
+unsafe fn ref_to_usize<T>(x: &T) -> usize {
+    core::mem::transmute(x)
+}
+unsafe fn usize_to_ref<'a, T>(x: usize) -> &'a mut T {
+    core::mem::transmute(x)
+}
+unsafe fn ref_dyn_fn_to_usize<T: ?Sized>(x: &dyn Fn(&mut T)) -> [usize; 2] {
+    core::mem::transmute(x)
+}
+unsafe fn usize_to_ref_dyn<'a, T: ?Sized>(x: [usize; 2]) -> &'a dyn Fn(&mut T) {
+    core::mem::transmute(x)
+}
+
+fn init_finish(futex: &Futex) {
+    use core::sync::atomic::Ordering::AcqRel;
+    if futex.fetch_sub(1, AcqRel) == 1 {
+        atomic_wait::wake_one(futex);
+    }
+}
+fn init_wait(futex: &Futex) {
+    loop {
+        let v = load!(futex);
+        if v == 0 {
+            break;
+        }
+        atomic_wait::wait(futex, v);
+    }
+}
+fn wait_cond<P>(futex: &Futex, pred: P) -> FutexInt
+where
+    P: Fn(FutexInt) -> bool,
+{
+    let mut val = load!(futex);
+    let mut count = 0;
+    while !pred(val) {
+        if count < 10000 {
+            thread::yield_now();
+        } else {
+            atomic_wait::wait(futex, val);
+        }
+        count += 1;
+        val = load!(futex);
+    }
+    val
+}
+struct JobWaiter<'a> {
+    count: usize,
+    futex: &'a Futex,
+    last_job_id: FutexInt,
+}
+impl<'a> JobWaiter<'a> {
+    pub fn new(futex: &'a Futex, init_job_id: FutexInt) -> Self {
+        Self {
+            count: 0,
+            futex,
+            last_job_id: init_job_id,
+        }
+    }
+    fn reset(&mut self) {
+        self.count = 0;
+    }
+    fn wait_yield(&mut self, val: FutexInt) {
+        if self.count < 10000 {
+            thread::yield_now();
+        } else {
+            atomic_wait::wait(self.futex, val);
+        }
+        self.count += 1;
+    }
+    pub fn wait_cond<F, P, T>(&mut self, mut load: F, mut pred: P) -> T
+    where
+        F: FnMut() -> T,
+        P: FnMut(&T) -> bool,
+    {
+        self.reset();
+        loop {
+            let new_job_id = load!(self.futex);
+            // Almost, `new_job_id > last_job_id`.
+            // Rerely, `new_job_id < last_job_id`.
+            if new_job_id != self.last_job_id {
+                self.last_job_id = new_job_id;
+                let v = load();
+                if pred(&v) {
+                    break v;
+                } else {
+                    continue;
+                }
+            }
+            self.wait_yield(new_job_id);
+        }
+    }
+}
+// 64 is cache line size, avoid false sharing
+#[repr(align(64))]
+struct Elem {
+    futex_end: Futex,
+    ptr: AtomicUsize,
+}
+#[repr(align(64))]
+struct FutexWrap(Futex);
+#[repr(align(64))]
+struct Main {
+    futex_start: Futex,
+    func: [AtomicUsize; 2],
+}
+/// static version thread pool
+pub struct ThreadPool<T, F>
+where
+    T: Send + Sync,
+    F: Fn(&mut T) + Clone + Send + Sync + 'static,
+{
+    threads: Vec<(JoinHandle<()>, Arc<Elem>)>,
+    futex_start: Arc<FutexWrap>,
+    func: F,
+    pd: PhantomData<T>,
+    unsync: PhantomUnsync,
+}
+impl<T, F> ThreadPool<T, F>
+where
+    T: Send + Sync,
+    F: Fn(&mut T) + Clone + Send + Sync + 'static,
+{
+    /// construst thread pool with function
+    ///
+    /// This function swpawns `nproc - 1` threads.
+    pub fn new(f: F) -> Self {
+        let cores = core_affinity::get_core_ids().unwrap();
+        let (_, t_core) = cores.split_first().unwrap();
+        let init_remain = Arc::new(Futex::new(t_core.len() as FutexInt));
+        let futex_start = Arc::new(FutexWrap(Futex::new(0)));
+        let threads = t_core
+            .iter()
+            .map(|core| {
+                let elem = Arc::new(Elem {
+                    futex_end: Futex::new(0),
+                    ptr: AtomicUsize::new(0),
+                });
+                let t = {
+                    clone_all!(f, futex_start, elem, core, init_remain);
+                    thread::spawn(move || {
+                        core_affinity::set_for_current(core);
+                        init_finish(&init_remain);
+                        let mut waiter = JobWaiter::new(&futex_start.0, 0);
+                        loop {
+                            let p = waiter.wait_cond(|| load!(elem.ptr), |&p| p != 0);
+                            // job execute
+                            f(unsafe { usize_to_ref(p) });
+                            store!(elem.ptr, 0);
+                            store!(elem.futex_end, 1);
+                            atomic_wait::wake_one(&elem.futex_end);
+                        }
+                    })
+                };
+                (t, elem)
+            })
+            .collect::<Vec<_>>();
+        init_wait(&init_remain);
+        Self {
+            threads,
+            futex_start,
+            func: f,
+            pd: PhantomData,
+            unsync: PhantomData,
+        }
+    }
+    /// returns max length of input of `run()`
+    pub fn max_len(&self) -> usize {
+        self.threads.len() + 1
+    }
+    /// run function which given `new()` with `data` on thread pool
+    ///
+    /// REQUIRE: `data.len() <= self.max_len()`
+    pub fn run(&self, data: &mut [T]) {
+        let (head, tail) = data.split_first_mut().unwrap();
+        let data_len = tail.len();
+        assert!(data_len <= self.threads.len(), "data is too long");
+        if data_len > 0 {
+            self.threads.iter().zip(tail).for_each(|(t, v)| {
+                store!(t.1.futex_end, 0); // set not end
+                store!(t.1.ptr, unsafe { ref_to_usize(v) }); // set data
+            });
+            // Worker threads do NOT modify `futex_start`. So, fetch_add is not needed.
+            unsafe { inc(&self.futex_start.0) };
+            atomic_wait::wake_all(&self.futex_start.0);
+        }
+        (self.func)(head);
+        if data_len > 0 {
+            self.threads.iter().take(data_len).for_each(|t| {
+                wait_cond(&t.1.futex_end, |x| x == 1);
+            });
+        }
+    }
+}
+/// dynamic version thread pool
+pub struct ThreadPoolDyn<T: Send + Sync> {
+    threads: Vec<(JoinHandle<()>, Arc<Elem>)>,
+    main: Arc<Main>,
+    pd: PhantomData<T>,
+    unsync: PhantomUnsync,
+}
+impl<T: Send + Sync> ThreadPoolDyn<T> {
+    /// construst thread pool
+    ///
+    /// This function swpawns `nproc - 1` threads.
+    pub fn new() -> Self {
+        let cores = core_affinity::get_core_ids().unwrap();
+        let (_, t_core) = cores.split_first().unwrap();
+        let init_remain = Arc::new(Futex::new(t_core.len() as FutexInt));
+        let main = Arc::new(Main {
+            futex_start: Futex::new(0),
+            func: [AtomicUsize::new(0), AtomicUsize::new(0)],
+        });
+        let threads = t_core
+            .iter()
+            .map(|core| {
+                let elem = Arc::new(Elem {
+                    futex_end: Futex::new(0),
+                    ptr: AtomicUsize::new(0),
+                });
+                let t = {
+                    clone_all!(main, elem, core, init_remain);
+                    thread::spawn(move || {
+                        core_affinity::set_for_current(core);
+                        init_finish(&init_remain);
+                        let mut waiter = JobWaiter::new(&main.futex_start, 0);
+                        loop {
+                            let d = waiter.wait_cond(|| load!(elem.ptr), |&p| p != 0);
+                            let f = load_pair(&main.func);
+                            let end = f[0] == 0 && f[1] == 0;
+                            if !end {
+                                let d = unsafe { usize_to_ref(d) };
+                                let f = unsafe { usize_to_ref_dyn::<T>(f) };
+                                f(d);
+                            }
+                            store!(elem.ptr, 0);
+                            store!(elem.futex_end, 1);
+                            atomic_wait::wake_one(&elem.futex_end);
+                            if end {
+                                break;
+                            }
+                        }
+                    })
+                };
+                (t, elem)
+            })
+            .collect::<Vec<_>>();
+        init_wait(&init_remain);
+        Self {
+            threads,
+            main,
+            pd: PhantomData,
+            unsync: PhantomData,
+        }
+    }
+    /// returns max length of input of `run()`
+    pub fn max_len(&self) -> usize {
+        self.threads.len() + 1
+    }
+    /// run `func` with `data` on thread pool
+    ///
+    /// REQUIRE: `data.len() <= self.max_len()`
+    pub fn run(&self, data: &mut [T], func: &(dyn Fn(&mut T) + Send + Sync)) {
+        debug_assert_eq!(load!(self.main.func[0]), 0);
+        debug_assert_eq!(load!(self.main.func[1]), 0);
+        let (head, tail) = data.split_first_mut().unwrap();
+        let data_len = tail.len();
+        assert!(data_len <= self.threads.len(), "data is too long");
+        if data_len > 0 {
+            let f = unsafe { ref_dyn_fn_to_usize(func) };
+            store_pair(&self.main.func, f);
+            self.threads.iter().zip(tail).for_each(|(t, v)| {
+                store!(t.1.futex_end, 0); // set not end
+                store!(t.1.ptr, unsafe { ref_to_usize(v) }); // set data
+            });
+            // Worker threads do NOT modify `futex_start`. So, fetch_add is not needed.
+            unsafe { inc(&self.main.futex_start) };
+            atomic_wait::wake_all(&self.main.futex_start);
+        }
+        func(head);
+        if data_len > 0 {
+            self.threads.iter().take(data_len).for_each(|t| {
+                wait_cond(&t.1.futex_end, |x| x == 1);
+            });
+            store_pair(&self.main.func, [0, 0]);
+        }
+        let _ = func;
+    }
+}
+impl<T: Send + Sync> Default for ThreadPoolDyn<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+impl<T: Send + Sync> Drop for ThreadPoolDyn<T> {
+    fn drop(&mut self) {
+        // assert_eq!(load!(self.main.func[0]), 0);
+        // assert_eq!(load!(self.main.func[1]), 0);
+        self.threads.iter().for_each(|t| {
+            store!(t.1.futex_end, 0);
+            store!(t.1.ptr, usize::MAX);
+        });
+        // worker threads do NOT modify `futex_start`. So, fetch_add is not needed.
+        unsafe { inc(&self.main.futex_start) };
+        atomic_wait::wake_all(&self.main.futex_start);
+        thread::yield_now();
+        self.threads.iter().for_each(|t| {
+            wait_cond(&t.1.futex_end, |x| x == 1);
+        });
+        // assert_eq!(self.threads.iter().filter(|t| !t.0.is_finished()).count(), 0);
+    }
+}
